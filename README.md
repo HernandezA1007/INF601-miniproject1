2c6edefdfa4340fb2aaa425b6da67ade3e87aaa9
HernandezA1007 <HernandezA1007@gmail.com>
2022-09-22 00:54:40 -0500

    Revised README file, added chosen stock tickers, attempted to use calendar and dividend data, savefig config.

--- README.md
+++ README.md
@@ -6,5 +6,40 @@
 - [ ] Quick start/steps - install and requirements
 - [ ] Build = execute
 
+# INF601 Mini Project 1
+
+### This project allows the user to receive images of stocks.
+The stocks provided are the projects owner example stocks, you will be required to know the stock ticker to receive
+a graph of your stock. 
+
+---
+## Quick Start
+
+> Before starting, look at requirements.txt to install the necessary packages
+
 `pip install -r requirements.txt`
 
+An example to startup the project can be taken below:
+```python
+import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = yf.download("MSFT", start="2022-08-30", end="2022-09-14")
+
+msftPrices = []
+
+for price in data['Adj Close']:
+    msftPrices.append(price)
+    
+print(msftPrices)
+
+msftarray = np.array(msftPrices)
+
+plt.plot(msftarray)
+
+plt.savefig('charts/msft.png')
+
+plt.show()
+
+```
--- main.py
+++ main.py
@@ -7,16 +7,31 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 
-# My stock choices are: SPY, NVDA, SBUX, PYPL, WMT, EX: MSFT
+# My stock choices are: SPY, NVDA, SBUX, PYPL, WMT, Ex: MSFT
 # Run > Receive PNG image > Save image file in another folder
-# terminal is not syncing with virtual environment
+# terminal is not syncing with virtual environment - not running
 
-data = yf.download("MSFT", start="2022-08-30", end="2022-09-14")
+myTickers = yf.Tickers('spy nvda sbux pypl wmt')
 
-msftPrices = []
+data = yf.download("MSFT", start="2022-08-30", end="2022-09-14")  # Ex
+myData = yf.download("SPY NVDA SBUX PYPL WMT", start="2022-08-30", end="2022-09-14",  auto_adjust = True,)
+
+msftPrices = []  # Ex
+spyPrices = []
+nvdaPrices = []
+sbuxPrices = []
+pyplPrices = []
+wmtPrices = []
+myPrices = []  # all ticker data?
+
 
 for price in data['Adj Close']:
     msftPrices.append(price)
+
+# Open, Close, calender, and dividends
+myTickers.tickers.NVDA.calendar  # shows next event
+myData.dividends  # shows all dividends?
+
 
 print(msftPrices)
 
@@ -28,6 +43,7 @@ plt.plot(msftarray)
 
 # Save the graph
 plt.savefig('charts/msft.png')
+plt.savefig('Charts/myData.png', facecolor='auto', edgecolor='auto', pad_inches=0.25)
 
 # Show the graph
 plt.show()  # Traceback error
