2f034f112ccff07c1d0f9ba274b7fd18ab8235e2
HernandezA1007 <HernandezA1007@gmail.com>
2022-09-22 14:00:54 -0500

    temp troubleshoot

--- main.py
+++ main.py
@@ -11,39 +11,40 @@ import matplotlib.pyplot as plt
 # Run > Receive PNG image > Save image file in another folder
 # terminal is not syncing with virtual environment - not running
 
-myTickers = yf.Tickers('spy nvda sbux pypl wmt')
+#myTickers = yf.Tickers('spy nvda sbux pypl wmt')
 
 data = yf.download("MSFT", start="2022-08-30", end="2022-09-14")  # Ex
-myData = yf.download("SPY NVDA SBUX PYPL WMT", start="2022-08-30", end="2022-09-14",  auto_adjust = True,)
+#myData = yf.download("SPY NVDA SBUX PYPL WMT", start="2022-08-30", end="2022-09-14",  auto_adjust = True,)
 
 msftPrices = []  # Ex
-spyPrices = []
-nvdaPrices = []
-sbuxPrices = []
-pyplPrices = []
-wmtPrices = []
-myPrices = []  # all ticker data?
+#spyPrices = []
+#nvdaPrices = []
+#sbuxPrices = []
+#pyplPrices = []
+#wmtPrices = []
+#myPrices = []  # all ticker data?
 
 
 for price in data['Adj Close']:
     msftPrices.append(price)
 
 # Open, Close, calender, and dividends
-myTickers.tickers.NVDA.calendar  # shows next event
-myData.dividends  # shows all dividends?
+#myTickers.tickers.NVDA.calendar  # shows next event
+#myData.dividends  # shows all dividends?
 
 
 print(msftPrices)
 
 # Create a NumPy array
 msftarray = np.array(msftPrices)
-
+print(msftarray)
 # Create matplotlib graph
 plt.plot(msftarray)
 
 # Save the graph
 plt.savefig('charts/msft.png')
-plt.savefig('Charts/myData.png', facecolor='auto', edgecolor='auto', pad_inches=0.25)
+#plt.savefig('Charts/myData.png', facecolor='auto', edgecolor='auto', pad_inches=0.25)
 
 # Show the graph
+
 plt.show()  # Traceback error
