f227b40e309d665ee0bf9e90aa6b1e0972b0800e
HernandezA1007 <HernandezA1007@gmail.com>
2022-09-21 23:29:59 -0500

    Added MSFT as an export PNG file.

--- main.py
+++ main.py
@@ -4,13 +4,29 @@
 
 
 import yfinance as yf
-# SPY, NVDA, SBUX, PYPL, WMT
+import numpy as np
+import matplotlib.pyplot as plt
+
+# My stock choices are: SPY, NVDA, SBUX, PYPL, WMT, EX: MSFT
+# Run > Receive PNG image > Save image file in another folder
 
 data = yf.download("MSFT", start="2022-08-30", end="2022-09-14")
-# print(data)
+
 msftPrices = []
 
 for price in data['Adj Close']:
     msftPrices.append(price)
 
 print(msftPrices)
+
+# Create a NumPy array
+msftarray = np.array(msftPrices)
+
+# Create matplotlib graph
+plt.plot(msftarray)
+
+# Save the graph
+plt.savefig('charts/msft.png')
+
+# Show the graph
+plt.show()  # Traceback error
